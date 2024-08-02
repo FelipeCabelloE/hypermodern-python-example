49cd60289a959730477b2efeda186e480195ecac
FelipeCabelloE <98831429+FelipeCabelloE@users.noreply.github.com>
2024-08-02 18:01:38 +0000

    second blog post Not yet finished

--- src/hypermodern_python_example/console.py
+++ src/hypermodern_python_example/console.py
@@ -3,18 +3,25 @@ import textwrap
 import click
 import requests
 
-from .const import VERSION
+from .const import __version__
+from . import wikipedia
 
 API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 
 
 @click.command()
-@click.version_option(version=VERSION)
-def main():
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
+@click.version_option(version=__version__)
+def main(language):
     """The hypermodern Python project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
